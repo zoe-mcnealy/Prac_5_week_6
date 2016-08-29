10df8dd4c217427ecfaf1ba5b2505b1b8155b3eb
jc232225 <zoe.mcnealy@my.jcu.edu.au>
2016-08-29 10:50:04 +1000

    last commit of the prac

--- colour_hex_codes.py
+++ colour_hex_codes.py
@@ -1,9 +1,11 @@
 HEX_COLOUR = {"AliceBlue": "#f0f8ff", "BlueViolet": "#8a2be2", "CadetBlue1": "#98f5ff", "chartreuse3": "#66cd00", "chocolate3": "#cd661d", "DarkGreen": "#006400", "DarkOliveGreen": "#556b2f", "DarkOrchid4": "#68228b", "HotPink1": "	#ff6eb4", "OrangeRed1": "#ff4500"}
 
 colour = input("Please enter a colours name.")
-while colour == "":
+while colour != "":
     if colour in HEX_COLOUR:
-        print(colour, "is", HEX_COLOUR.values(colour))
+        print(colour, "is", HEX_COLOUR[colour])
+        colour = input("Please enter a colours name.")
+
     else:
         print("That colour is not in my dictionary. Please try again")
         colour = input("Please enter a colours name.")
--- string_counter.py
+++ string_counter.py
@@ -1,10 +1,11 @@
 text = "this is a collection of nice words this is a fun thing it is"
 
+
 individual_words = text.lower() .split(" ")
 text_dict = {}
 for word in individual_words:
     text_dict[word] = individual_words.count(word)
 print(text_dict)
-sorted(text_dict.keys())
+sorted(text_dict)
 for k,v in text_dict.items():
     print("{:{}}:{:{}}".format(k,10,v,2))
